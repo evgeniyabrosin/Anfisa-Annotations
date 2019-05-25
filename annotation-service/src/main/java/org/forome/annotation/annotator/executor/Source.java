cc8d8101efd6a0337053683bc5eece847cf13d2b
Vladimir A. Ulitin <v.ulitin@mobin.me>
2019-05-25 09:29:23 +0300

    fix

--- annotation-service/src/main/java/org/forome/annotation/annotator/executor/Source.java
+++ annotation-service/src/main/java/org/forome/annotation/annotator/executor/Source.java
@@ -4,7 +4,6 @@ import htsjdk.variant.variantcontext.VariantContext;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
-import org.forome.annotation.connector.anfisa.AnfisaConnector;
 import org.forome.annotation.controller.utils.RequestParser;
 import org.forome.annotation.exception.ExceptionBuilder;
 
@@ -31,10 +30,35 @@ class Source {
 
             //Для оптимизации валидируем тут - конечно было бы правильнее сделать это выше,
             // но тогда будет проседание по производительности
-            String vepJsonChromosome = RequestParser.toChromosome(AnfisaConnector.getChromosome(_vepJson));
+            String[] vepJsonInput = _vepJson.getAsString("input").split("\t");
+
             String vcfChromosome = RequestParser.toChromosome(variantContext.getContig());
-            if (!vepJsonChromosome.equals(vcfChromosome)) {
-                throw new RuntimeException("Not equals chromosome, vcf and vep.json, vcf: " + vcfChromosome);
+            String vepJsonChromosome = RequestParser.toChromosome(vepJsonInput[0]);
+            if (!vcfChromosome.equals(vepJsonChromosome)) {
+                throw new RuntimeException(
+                        String.format("Not equals chromosome, vcf %s and vep.json %s", vcfChromosome, vepJsonChromosome)
+                );
+            }
+
+            String vcfId = variantContext.getID();
+            String vepJsonId = vepJsonInput[2];
+            if (!vcfId.equals(vepJsonId)) {
+                throw new RuntimeException(
+                        String.format("Not equals id, vcf %s and vep.json %s", vcfId, vepJsonId)
+                );
+            }
+
+            int vcfStart = variantContext.getStart();
+            int vcfEnd = variantContext.getEnd();
+            int vepJsonPosition = Integer.parseInt(vepJsonInput[1]);
+            if (!(
+                    Math.min(vcfStart, vcfEnd) <= vepJsonPosition && vepJsonPosition <= Math.max(vcfStart, vcfEnd)
+            )) {
+                throw new RuntimeException(
+                        String.format("Not equals: vcf start: %s, vcf end: %s, vep.json position: %s",
+                                vcfStart, vcfEnd, vepJsonPosition
+                        )
+                );
             }
         }
         return _vepJson;
