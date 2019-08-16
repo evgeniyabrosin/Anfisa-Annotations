de80f8809c0a687083bee1e7c6bbdf0d322887f8
Vladimir A. Ulitin <v.ulitin@mobin.me>
2019-08-16 20:18:06 +0300

    refactoring

--- annotation-service/src/main/java/org/forome/annotation/annotator/struct/AnnotatorResult.java
+++ annotation-service/src/main/java/org/forome/annotation/annotator/struct/AnnotatorResult.java
@@ -50,7 +50,7 @@ public class AnnotatorResult {
                 metadataDatabases.addAll(anfisaConnector.hgmdConnector.getMetadata());
                 metadataDatabases.addAll(anfisaConnector.spliceAIConnector.getMetadata());
                 metadataDatabases.addAll(anfisaConnector.conservationConnector.getMetadata());
-                metadataDatabases.addAll(anfisaConnector.gnomadConnector.getMetadata());
+//                metadataDatabases.addAll(anfisaConnector.gnomadConnector.getMetadata());
                 metadataDatabases.sort(Comparator.comparing(o -> o.product));
             }
         }
