82274c95cae916b6a4acc3173bf11d664cd817d4
Vladimir A. Ulitin <v.ulitin@mobin.me>
2019-08-29 15:18:21 +0300

    fix increment version: 0.5.0.28

--- annotation-service/src/main/java/org/forome/annotation/annotator/main/AnnotatorMainFork.java
+++ annotation-service/src/main/java/org/forome/annotation/annotator/main/AnnotatorMainFork.java
@@ -1,7 +1,9 @@
 package org.forome.annotation.annotator.main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -30,14 +32,57 @@ public class AnnotatorMainFork {
         processBuilder.directory(Paths.get(".").toAbsolutePath().toFile());
         Process process = processBuilder.start();
 
+        //Читаем stdout
+        StringBuilder stdout = new StringBuilder();
+        Thread threadStdOut = new Thread(() -> {
+            try (InputStreamReader inputReader = new InputStreamReader(process.getInputStream())) {
+                String line;
+                try (BufferedReader bufferedReader = new BufferedReader(inputReader)) {
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stdout.append(line).append(System.lineSeparator());
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        threadStdOut.setDaemon(true);
+        threadStdOut.start();
+
+        //Читаем stderr
+        StringBuilder stderr = new StringBuilder();
+        Thread threadStdErr = new Thread(() -> {
+            try (InputStreamReader inputReader = new InputStreamReader(process.getErrorStream())) {
+                String line;
+                try (BufferedReader bufferedReader = new BufferedReader(inputReader)) {
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stderr.append(line).append(System.lineSeparator());
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        threadStdErr.setDaemon(true);
+        threadStdErr.start();
+
+
         //Немного ждем - убеждаемся, что все работам и выходим
-        boolean complete = process.waitFor(5, TimeUnit.SECONDS);
+        boolean complete = process.waitFor(7, TimeUnit.SECONDS);
         if (complete) {
             int exitCode = process.exitValue();
             if (exitCode == 0) {
                 System.out.println("Process completed successfully");
             } else {
                 System.out.println("The process ended with an error!!! ExitCode: " + exitCode);
+                if (stdout.length() > 0) {
+                    System.out.println("stdout: ");
+                    System.out.println(stdout.toString());
+                }
+                if (stderr.length() > 0) {
+                    System.out.println("stderr: ");
+                    System.out.println(stderr.toString());
+                }
             }
         }
     }
