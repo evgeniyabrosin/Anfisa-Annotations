45fbe04545deaa0a35a1dbb52f92697ceee300dc
Vladimir A. Ulitin <v.ulitin@mobin.me>
2019-12-03 16:35:59 +0300

    new variant type: substitution new version: 0.5.0.86

--- annotation-service/src/main/java/org/forome/annotation/iterator/vcf/VCFFileIterator.java
+++ annotation-service/src/main/java/org/forome/annotation/iterator/vcf/VCFFileIterator.java
@@ -17,60 +17,54 @@ import java.util.NoSuchElementException;
 
 public class VCFFileIterator implements AutoCloseable {
 
-    private final static Logger log = LoggerFactory.getLogger(VCFFileIterator.class);
+	private final static Logger log = LoggerFactory.getLogger(VCFFileIterator.class);
 
-    private final VCFFileReader vcfFileReader;
-    private final CloseableIterator<VariantContext> vcfFileReaderIterator;
+	private final VCFFileReader vcfFileReader;
+	private final CloseableIterator<VariantContext> vcfFileReaderIterator;
 
-    private final CNVFileIterator cnvFileIterator;
+	private final CNVFileIterator cnvFileIterator;
 
-    public VCFFileIterator(Path pathVcf) {
-        this(pathVcf, null);
-    }
+	public VCFFileIterator(Path pathVcf) {
+		this(pathVcf, null);
+	}
 
-    public VCFFileIterator(Path pathVcf, Path cnvFile) {
-        this.vcfFileReader = new VCFFileReader(pathVcf, false);
-        this.vcfFileReaderIterator = vcfFileReader.iterator();
+	public VCFFileIterator(Path pathVcf, Path cnvFile) {
+		this.vcfFileReader = new VCFFileReader(pathVcf, false);
+		this.vcfFileReaderIterator = vcfFileReader.iterator();
 
-        if (cnvFile != null) {
-            cnvFileIterator = new CNVFileIterator(cnvFile);
+		if (cnvFile != null) {
+			cnvFileIterator = new CNVFileIterator(cnvFile);
 
-            //Validation equals samples
-            List<String> vcfSamples = vcfFileReader.getFileHeader().getGenotypeSamples();
-            List<String> cnvSamples = cnvFileIterator.getSamples();
-            if (vcfSamples.size() != cnvSamples.size() || !vcfSamples.containsAll(cnvSamples)) {
-                throw ExceptionBuilder.buildNotEqualSamplesVcfAndCnvFile();
-            }
-        } else {
-            cnvFileIterator = null;
-        }
-    }
+			//Validation equals samples
+			List<String> vcfSamples = vcfFileReader.getFileHeader().getGenotypeSamples();
+			List<String> cnvSamples = cnvFileIterator.getSamples();
+			if (vcfSamples.size() != cnvSamples.size() || !vcfSamples.containsAll(cnvSamples)) {
+				throw ExceptionBuilder.buildNotEqualSamplesVcfAndCnvFile();
+			}
+		} else {
+			cnvFileIterator = null;
+		}
+	}
 
-    public VariantVep next() throws NoSuchElementException {
-        while (true) {
-            if (vcfFileReaderIterator.hasNext()) {
-                VariantContext variantContext;
-                try {
-                    variantContext = vcfFileReaderIterator.next();
-                } catch (Throwable e) {
-                    log.warn("Invalid variant, ignored", e);
-                    continue;
-                }
-                if (Chromosome.CHR_M == Chromosome.of(variantContext.getContig())) {
-                    continue;//Игнорируем митохондрии
-                }
-                return new VariantVCF(variantContext);
-            } else if (cnvFileIterator != null && cnvFileIterator.hasNext()) {
-                return cnvFileIterator.next();
-            } else {
-                throw new NoSuchElementException();
-            }
-        }
-    }
+	public VariantVep next() throws NoSuchElementException {
+		while (true) {
+			if (vcfFileReaderIterator.hasNext()) {
+				VariantContext variantContext = vcfFileReaderIterator.next();
+				if (Chromosome.CHR_M == Chromosome.of(variantContext.getContig())) {
+					continue;//Игнорируем митохондрии
+				}
+				return new VariantVCF(variantContext);
+			} else if (cnvFileIterator != null && cnvFileIterator.hasNext()) {
+				return cnvFileIterator.next();
+			} else {
+				throw new NoSuchElementException();
+			}
+		}
+	}
 
-    @Override
-    public void close() {
-        this.vcfFileReaderIterator.close();
-        this.vcfFileReader.close();
-    }
+	@Override
+	public void close() {
+		this.vcfFileReaderIterator.close();
+		this.vcfFileReader.close();
+	}
 }
