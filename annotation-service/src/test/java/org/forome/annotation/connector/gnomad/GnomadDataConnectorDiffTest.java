041d518fb2610ce3e495444cb65341c0844e1924
Vladimir A. Ulitin <v.ulitin@mobin.me>
2020-01-19 10:10:28 +0300

    fix new version: 0.5.0.90

--- annotation-service/src/test/java/org/forome/annotation/connector/gnomad/GnomadDataConnectorDiffTest.java
+++ annotation-service/src/test/java/org/forome/annotation/connector/gnomad/GnomadDataConnectorDiffTest.java
@@ -1,16 +1,10 @@
 package org.forome.annotation.connector.gnomad;
 
-import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.vcf.VCFFileReader;
 import org.forome.annotation.connector.gnomad.old.GnomadDataConnectorOld;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Iterator;
 
 public class GnomadDataConnectorDiffTest {
 
@@ -24,19 +18,6 @@ public class GnomadDataConnectorDiffTest {
         Assert.assertEquals("-CAT", GnomadDataConnectorOld.diff("CCATCAT", "CCAT"));
         Assert.assertEquals("-CATCAT", GnomadDataConnectorOld.diff("CCATCATCATCAT", "CCATCAT"));
         Assert.assertEquals("CAT", GnomadDataConnectorOld.diff("CCATCATCATCAT", "CCATCATCATCATCAT"));
-    }
-
-    @Test
-    public void test1() throws Exception {
-        Path file = Paths.get("/home/kris/processtech/tmp/bch0004_wgs_2.vcf");
-        VCFFileReader vcfFileReader = new VCFFileReader(file, false);
-
-        Iterator<VariantContext> iterator = vcfFileReader.iterator();
-        while (iterator.hasNext()) {
-            VariantContext variantContext = iterator.next();
-            log.debug("Variant: {}", variantContext);
-        }
-        vcfFileReader.close();
     }
 
 }
