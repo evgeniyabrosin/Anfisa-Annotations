01b96fe2d62f00ea24ab36532d4bd643735d4479
Artem Sveshnikov <artem.sveshnikov@quantori.com>
2021-08-04 12:53:15 +0300

    Added possibility vep-annotation in docker

---
+++ pipeline/projects/ensembl-vep/build_incontainer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Run the initial procedure to download and cache Ensembl VEP data for the particular release
+
+set -euxo pipefail
+
+source env.sh
+
+if [ -d $VEP_CACHE_DIR ]; then
+    echo "Forome VEP setup is already built. Remove $VEP_CACHE_DIR cache directory to generate a new build."
+    exit 1
+fi
+
+echo "Download VEP cache files"
+
+mkdir -p $VEP_CACHE_DIR
+chmod a+rwx $VEP_CACHE_DIR
+
+perl INSTALL.pl -a p -g $VEP_PLUGINS -c $(realpath $VEP_CACHE_DIR)
+perl INSTALL.pl -a cf -s $VEP_SPECIES -y $VEP_ASSEMBLY -c $(realpath $VEP_CACHE_DIR)
+
+echo "Download Plugins config files"
+
+VEP_RELEASE_SHORT=$(echo ${VEP_RELEASE}|awk -F'[_.]' '{print$2}')
+
+wget https://raw.github.com/Ensembl/VEP_plugins/release/${VEP_RELEASE_SHORT}/ExACpLI_values.txt -o /dev/null -O ${VEP_CACHE_DIR}/Plugins/ExACpLI_values.txt
+wget http://hollywood.mit.edu/burgelab/maxent/download/fordownload.tar.gz -o /dev/null && tar xzf fordownload.tar.gz -C ${VEP_CACHE_DIR}/Plugins/ && rm fordownload.tar.gz
+wget https://raw.github.com/Ensembl/VEP_plugins/release/${VEP_RELEASE_SHORT}/LoFtool_scores.txt -o /dev/null -O ${VEP_CACHE_DIR}/Plugins/LoFtool_scores.txt
+
+echo "Forome VEP setup is done"
---
+++ pipeline/projects/ensembl-vep/run_incontainer.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+set -euo pipefail
+
+if [ $# -eq 0 ]; then
+    echo "Please supply an input file as an argument"
+    exit 1
+fi
+
+INPUT_FULL_PATH=$(realpath $1)
+DIR_PATH=$(dirname ${INPUT_FULL_PATH})
+
+INPUT_FILE=$(basename ${INPUT_FULL_PATH})
+OUTPUT_FILE="$(basename $INPUT_FILE .vcf).vep.json"
+
+OUTPUT_FULL_PATH="${DIR_PATH}/${OUTPUT_FILE}"
+
+VEP_SETUP_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source ${VEP_SETUP_DIR}/env.sh
+
+if [ ! -d $VEP_CACHE_DIR ]; then
+    echo "$VEP_CACHE_DIR does not exist! Run `build.sh` to download the cache files for these parameters."
+    exit 1
+fi
+
+if test -f "${OUTPUT_FULL_PATH}"; then
+    echo "${OUTPUT_FULL_PATH} exists! Remove the file to make a new VEP annotation."
+    exit 1
+else
+    echo "VEP annotation: ${INPUT_FULL_PATH} -> ${OUTPUT_FULL_PATH}"
+
+    vep \
+    --buffer_size 50000 \
+    --cache --dir /opt/vep/.vep --dir_cache /opt/vep/.vep \
+    --fork 4 \
+    --force_overwrite \
+    --merged \
+    --json \
+    --port 3337 \
+    --input_file /data/${INPUT_FILE} \
+    --output_file /data/${OUTPUT_FILE} \
+    --plugin ExACpLI,/opt/vep/.vep/Plugins/ExACpLI_values.txt \
+    --plugin MaxEntScan,/opt/vep/.vep/Plugins/fordownload \
+    --plugin LoFtool,/opt/vep/.vep/Plugins/LoFtool_scores.txt \
+    --plugin SpliceRegion \
+    --everything
+fi
