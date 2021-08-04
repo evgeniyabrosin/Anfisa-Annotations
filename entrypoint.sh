30bb7fbae377d6a4d8f6fd355ddcc8f55f385b31
Artem Sveshnikov <artem.sveshnikov@quantori.com>
2021-08-04 15:29:46 +0300

    Changed entrypoint.sh for configure vep

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,8 @@ then
     source /data/project/AStorage/Anfisa-Annotations/pipeline/projects/ensembl-vep/env.sh
     exec uwsgi --ini /data/project/AStorage/uwsgi.ini
 fi
+
+if [ "$1" = "configurevep" ] ;
+then
+    exec /data/project/AStorage/Anfisa-Annotations/pipeline/projects/ensembl-vep/build_incontainer.sh
+fi
