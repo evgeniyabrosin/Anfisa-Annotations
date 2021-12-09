1c3c128b98c686cdcb1599b4cb37101fc0114b10
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-12-09 19:33:59 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "astorage" ] ;
 then
     source /data/project/AStorage/Anfisa-Annotations/pipeline/projects/ensembl-vep/env.sh
-#    exec uwsgi --ini /data/project/AStorage/uwsgi.ini --virtualenv /data/project/AStorage/Anfisa-Annotations/venv/
+#    exec uwsgi --ini /data/project/AStorage/uwsgi.ini --virtualenv /data/project/AStorage/venv
     exec uwsgi --ini /data/project/AStorage/uwsgi.ini
 fi
 if [ "$1" = "configurevep" ] ;
