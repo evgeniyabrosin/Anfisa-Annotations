5a903598d9db48615e5827683149bd77ef1afb1b
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-12-14 20:44:18 +0300

    Update env_incontainer.sh

--- pipeline/projects/ensembl-vep/env_incontainer.sh
+++ pipeline/projects/ensembl-vep/env_incontainer.sh
@@ -1,7 +1,7 @@
 VEP_SPECIES=homo_sapiens_merged
 VEP_ASSEMBLY=GRCh37
 VEP_PLUGINS="ExACpLI,MaxEntScan,LoFtool,SpliceRegion"
-VEP_RELEASE=release_103
+VEP_RELEASE=release_105.0
 VEP_IMAGE=ensemblorg/ensembl-vep:${VEP_RELEASE}
 VEP_DIR=/data/vep
 VEP_CACHE_DIR=/opt/vep/.vep/
